c867dfecfa9f377d7d4c3f8a5efc7973cb5ed683
Kieran Cornwall <kcorn1982@gmail.com>
2021-09-03 11:17:36 +0100

    Creating our app config and config reader object

---
+++ src/main/java/config_mgr/ConfigMgr.java
@@ -0,0 +1,23 @@
+package config_mgr;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigMgr {
+    private static final Properties config;
+
+    static {
+        config = new Properties();
+        try {
+            config.load(new FileReader("src/main/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String EmployeeFileLocation(){
+        return config.getProperty("employee_file_location");
+    }
+
+}
